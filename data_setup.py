7ccc2f3fffde353e7eabc44f6fedb499f743ac4c
michaelgu95 <michaelgu95@gmail.com>
2015-12-28 16:56:58 -0500

    all features added

--- data_setup.py
+++ data_setup.py
@@ -13,18 +13,91 @@ import re
 style.use("dark_background")
 path = '/Users/michaelgu/Documents/scikit-learn-investing/data'
 
-def Key_Stats(gather="Total Debt/Equity (mrq)"):
+# define funciton with gather as list of features we are interested in 
+def Key_Stats(gather=["Total Debt/Equity",
+                      'Trailing P/E',
+                      'Price/Sales',
+                      'Price/Book',
+                      'Profit Margin',
+                      'Operating Margin',
+                      'Return on Assets',
+                      'Return on Equity',
+                      'Revenue Per Share',
+                      'Market Cap',
+                        'Enterprise Value',
+                        'Forward P/E',
+                        'PEG Ratio',
+                        'Enterprise Value/Revenue',
+                        'Enterprise Value/EBITDA',
+                        'Revenue',
+                        'Gross Profit',
+                        'EBITDA',
+                        'Net Income Avl to Common ',
+                        'Diluted EPS',
+                        'Earnings Growth',
+                        'Revenue Growth',
+                        'Total Cash',
+                        'Total Cash Per Share',
+                        'Total Debt',
+                        'Current Ratio',
+                        'Book Value Per Share',
+                        'Cash Flow',
+                        'Beta',
+                        'Held by Insiders',
+                        'Held by Institutions',
+                        'Shares Short (as of',
+                        'Short Ratio',
+                        'Short % of Float',
+                        'Shares Short (prior ']):
+
 		statspath = path+'/_KeyStats'
 		stock_list = [x[0] for x in os.walk(statspath)]
 		df = pd.DataFrame(columns = ['Date',
-									 'Unix',
-									 'Ticker',
-									 'DE Ratio',
-									 'Price',
-									 'stock_p_change',
-									 'SP500',
-							         'sp500_p_change',
-							         'Difference'])
+                                 'Unix',
+                                 'Ticker',
+                                 'Price',
+                                 'stock_p_change',
+                                 'SP500',
+                                 'sp500_p_change',
+                                 'Difference',
+                                 ##############
+                                 'DE Ratio',
+                                 'Trailing P/E',
+                                 'Price/Sales',
+                                 'Price/Book',
+                                 'Profit Margin',
+                                 'Operating Margin',
+                                 'Return on Assets',
+                                 'Return on Equity',
+                                 'Revenue Per Share',
+                                 'Market Cap',
+                                 'Enterprise Value',
+                                 'Forward P/E',
+                                 'PEG Ratio',
+                                 'Enterprise Value/Revenue',
+                                 'Enterprise Value/EBITDA',
+                                 'Revenue',
+                                 'Gross Profit',
+                                 'EBITDA',
+                                 'Net Income Avl to Common ',
+                                 'Diluted EPS',
+                                 'Earnings Growth',
+                                 'Revenue Growth',
+                                 'Total Cash',
+                                 'Total Cash Per Share',
+                                 'Total Debt',
+                                 'Current Ratio',
+                                 'Book Value Per Share',
+                                 'Cash Flow',
+                                 'Beta',
+                                 'Held by Insiders',
+                                 'Held by Institutions',
+                                 'Shares Short (as of',
+                                 'Short Ratio',
+                                 'Short % of Float',
+                                 'Shares Short (prior ',                                
+                                 ##############
+                                 'Status'])
 
 		sp500_df = pd.DataFrame.from_csv("YAHOO-INDEX_GSPC.csv")
 		ticker_list = []
@@ -45,7 +118,24 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
 					# open html file for scraping
 					source = codecs.open(full_file_path,'r', 'utf-8').read()
 					try:
-						value = float(source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
+						# list to store feature values
+						value_list = []
+						for each_data in gather:
+							try:
+								regex = re.escape(each_data) + r'.*?(\d{1,8}\.\d{1,8}M?B?|N/A)%?</td>'
+								value = re.search(regex, source)
+								value = value.group(1)
+
+								if ("B") in value:
+									value = float(value.replace("B",''))*1000000000
+								elif "M" in value:
+									value = float(value.replace("M",''))*1000000
+								value_list.append(value)
+
+
+							except Exception as e:
+								value = "N/A"
+								value_list.append(value)
 						# get SP500 value 
 						try:
 							sp500_date = datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d')
@@ -55,49 +145,112 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
 							sp500_date = datetime.fromtimestamp(unix_time-259200).strftime('%Y-%m-%d')
 							row = sp500_df[(sp500_df.index == sp500_date)]
 							sp500_value = float(row["Adj Close"])
+
+
 						# scrape stock price 
 						soup = BeautifulSoup(source,'lxml')
-						for tag in soup.findAll('b'):
-							if tag.parent.name == 'big':
-								if tag.text != "Key Statistics":
-									stock_price = float(tag.text) 	
+						try:
+							for tag in soup.findAll('b'):
+								if tag.parent.name == 'big':
+									if tag.text != "Key Statistics":
 
+										stock_price = float(tag.text) 
+										stock_price = float(re.search(r'(\d{1,8}\.\d{1,8})',stock_price))
+										stock_price = float(stock_price.group(1))	
+						except Exception as e:
+							# tag = soup.find_all('span', {'class':'time_rtq_ticker'})
+							# print(tag)
+							# stock_price = float(tag.firstChild().text) 
+							# print(stock_price)
+							# stock_price = re.search(r'(\d{1,8}\.\d{1,8})',stock_price)
+							# stock_price = float(stock_price.group(1))
+							pass
 
+						# set starting values
 						if not starting_stock_value:
 								starting_stock_value = stock_price
 						if not starting_sp500_value:
 								starting_sp500_value = sp500_value
-
+						# calculate percent changes
 						stock_p_change = ((stock_price - starting_stock_value)/starting_stock_value)*100
 						sp500_p_change = ((sp500_value - starting_sp500_value)/starting_sp500_value)*100
+						
+						difference = stock_p_change-sp500_p_change
+						if difference > 0:
+							status = "outperform"
+						else:
+							status = "underperform"
 
-						# append to panda dataframe
-						df = df.append({'Date':date_stamp,
-										'Unix':unix_time,
-										'Ticker':ticker,
-										'DE Ratio':value,
-										'Price':stock_price,
-										'stock_p_change':stock_p_change,
-										'SP500':sp500_value,
-										'sp500_p_change':sp500_p_change,
-										'Difference':stock_p_change-sp500_p_change}, ignore_index = True)
+						if value_list.count("N/A") > 0:
+							pass
+						else:
+							# append scraped data values to panda dataframe
+							df = df.append({'Date':date_stamp,
+											'Unix':unix_time,
+											'Ticker':ticker,
+											'DE Ratio':value,
+											'Price':stock_price,
+											'stock_p_change':stock_p_change,
+											'SP500':sp500_value,
+											'sp500_p_change':sp500_p_change,
+											'Difference':difference,
+											'DE Ratio':value_list[0],
+                                            #'Market Cap':value_list[1],
+                                            'Trailing P/E':value_list[1],
+                                            'Price/Sales':value_list[2],
+                                            'Price/Book':value_list[3],
+                                            'Profit Margin':value_list[4],
+                                            'Operating Margin':value_list[5],
+                                            'Return on Assets':value_list[6],
+                                            'Return on Equity':value_list[7],
+                                            'Revenue Per Share':value_list[8],
+                                            'Market Cap':value_list[9],
+                                            'Enterprise Value':value_list[10],
+                                            'Forward P/E':value_list[11],
+                                            'PEG Ratio':value_list[12],
+                                            'Enterprise Value/Revenue':value_list[13],
+                                            'Enterprise Value/EBITDA':value_list[14],
+                                            'Revenue':value_list[15],
+                                            'Gross Profit':value_list[16],
+                                            'EBITDA':value_list[17],
+                                            'Net Income Avl to Common ':value_list[18],
+                                            'Diluted EPS':value_list[19],
+                                            'Earnings Growth':value_list[20],
+                                            'Revenue Growth':value_list[21],
+                                            'Total Cash':value_list[22],
+                                            'Total Cash Per Share':value_list[23],
+                                            'Total Debt':value_list[24],
+                                            'Current Ratio':value_list[25],
+                                            'Book Value Per Share':value_list[26],
+                                            'Cash Flow':value_list[27],
+                                            'Beta':value_list[28],
+                                            'Held by Insiders':value_list[29],
+                                            'Held by Institutions':value_list[30],
+                                            'Shares Short (as of':value_list[31],
+                                            'Short Ratio':value_list[32],
+                                            'Short % of Float':value_list[33],
+                                            'Shares Short (prior ':value_list[34],
+                                            'Status':status}, ignore_index = True)
 					except Exception as e:
 						pass
+			# notify
 			print('indexed data for:', each_dir)
 
 		# plot the data
-		for each_ticker in ticker_list:
-			try:
-				plot_df = df[(df['Ticker'] == each_ticker)]
-				plot_df = plot_df.set_index(['Date'])
-				plot_df['Difference'].plot(label=each_ticker)
-				plt.legend()
-			except:
-				pass
-		plt.show()
-
-		save = gather.replace(' ','').replace(')','').replace('(','').replace('/','')+('.csv')
-		print(save)
-		df.to_csv(save)
+		# for each_ticker in ticker_list:
+		# 	try:
+		# 		plot_df = df[(df['Ticker'] == each_ticker)]
+		# 		plot_df = plot_df.set_index(['Date'])
+		# 		if plot_df['Status'][-1] == "underperform":
+		# 			color = 'r'
+		# 		else:
+		# 			color = 'g'
+		# 		plot_df['Difference'].plot(label=each_ticker, color=color)
+		# 		plt.legend()
+		# 	except:
+		# 		pass
+		# plt.show()
+		
+		df.to_csv("key_stats.csv")
 
 Key_Stats()
---
+++ linear_svc.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+from matplotlib import style
+style.use("ggplot")
+
